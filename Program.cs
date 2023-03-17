d91644303b37a44ada8d9e3ad5c48799d9f5a6cd
chapcs <benchapman003@gmail.com>
2023-03-17 08:39:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class PaintballGun
+{
+    public const int MAGAZINE_SIZE = 16;
+
+    private int balls = 0;
+    private int ballsLoaded = 0;
+
+    public int GetBallsLoaded() { return ballsLoaded; }
+
+    public bool IsEmpty() { return ballsLoaded == 0; }
+
+
+}
