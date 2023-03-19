0d2925e8fa4f2e57abe1b6234ae11414e7704595
chapcs <benchapman003@gmail.com>
2023-03-19 11:03:19 -0400

    Main method completed

--- Program.cs
+++ Program.cs
@@ -2,7 +2,23 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        PaintballGun gun = new PaintballGun();
+        while (true)
+        {
+            Console.WriteLine($"{gun.GetBalls()} balls, {gun.GetBallsLoaded()} loaded");
+            if (gun.IsEmpty())
+                Console.WriteLine("WARNING: out of ammo");
+            Console.WriteLine("Space to shoot, r to reload, + to add ammo, q to quit");
+            char key = Console.ReadKey(true).KeyChar;
+            if (key == ' ')
+                Console.WriteLine($"Shooting returned {gun.Shoot()}");
+            else if (key == 'r')
+                gun.Reload();
+            else if (key == '+')
+                gun.SetBalls(gun.GetBalls() + PaintballGun.MAGAZINE_SIZE);
+            else if (key == 'q') 
+                return;
+        }
     }
 }
 
@@ -15,7 +31,7 @@ class PaintballGun
 
     public int GetBallsLoaded() { return ballsLoaded; }
 
-    public bool IsEmpty() { return ballsLoaded == 0; }
+    public bool IsEmpty() { return ballsLoaded == 0; } //redundant
 
     public int GetBalls() { return balls; }
 
